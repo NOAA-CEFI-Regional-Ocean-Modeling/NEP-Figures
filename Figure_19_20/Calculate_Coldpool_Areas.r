e8f97364e67cd8cf7ceab40e09212692d71665cc
Utheri Wagura <utheriwagura@gmail.com>
2025-04-25 10:03:03 -0400

    Last changes were not commit correctly, recomitting

--- Figure_19_20/Calculate_Coldpool_Areas.r
+++ Figure_19_20/Calculate_Coldpool_Areas.r
@@ -2,21 +2,24 @@ library(coldpool)
 library(akgfmaps)
 
 proj_crs <- coldpool:::ebs_proj_crs
-ebs_nep_csv_path <- '/work/Utheri.Wagura/NEP/plotting/Figure_19_20/index_hauls_temperature_data_nep.csv'
+experiment <- "no_bias_correction"
+experiment_dir <- paste('/work/Utheri.Wagura/NEP/plotting/Figure_19_20',experiment, sep='/')
+ebs_nep_csv_path <- paste( experiment_dir, 'index_hauls_temperature_data_nep.csv', sep='/')
+print( paste("Working with the following ebs file:",ebs_nep_csv_path, sep = " " ) )
 
 # Interpolate gear temperature and write rasters for SEBS - nep bottom temp values
 print("Interpolating index hauls")
 interpolation_wrapper(temp_data_path = ebs_nep_csv_path,
                       proj_crs = proj_crs,
                       cell_resolution = 5000, # 5x5 km grid resolution
-                      select_years = 1993:2024,
+                      select_years = 1993:2019,
                       interp_variable = "nep_tob",
                       select_region = "sebs",
-                      methods = "Ste") 
+                      methods = "Ste")
 
 # Calculate cold pool area for NEP
 print("Calculate Cold Pool Area for NEP")
-bottom_temp_files <- list.files(here::here("output", "raster", "sebs", "nep_tob"), 
+bottom_temp_files <- list.files(here::here("output", "raster", "sebs", "nep_tob"),
                                 full.names = TRUE,
                                 pattern = "ste_")
 
@@ -33,10 +36,10 @@ bt_df <- data.frame(YEAR = numeric(length = length(bottom_temp_files)),
 print("Setting up mask to calculate mean bottom temp")
 ebs_layers <- akgfmaps::get_base_layers(select.region = "sebs", set.crs = proj_crs)
 
-print("Creatin lt100 strata")
+print("Creating lt100 strata")
 lt100_strata <- ebs_layers$survey.strata |>
-  dplyr::filter(Stratum %in% c(10, 20, 31, 32, 41, 42, 43)) |>
-  dplyr::group_by(SURVEY) |>
+  dplyr::filter(STRATUM %in% c(10, 20, 31, 32, 41, 42, 43)) |>
+  dplyr::group_by(SURVEY_DEFINITION_ID) |>
   dplyr::summarise()
 
 print("Looping over bottom temp files")
@@ -44,28 +47,37 @@ for(i in 1:length(bottom_temp_files)) {
   print(i)
   bt_raster <- terra::rast(bottom_temp_files[i])
   bt_df$YEAR[i] <- as.numeric(gsub("[^0-9.-]", "", names(bt_raster))) # Extract year
-  bt_df$AREA_LTE2_KM2[i] <- bt_raster |> 
+  bt_df$AREA_LTE2_KM2[i] <- bt_raster |>
     cpa_from_raster(raster_units = "m", temperature_threshold = 2)
-  bt_df$AREA_LTE1_KM2[i] <- bt_raster |> 
+  bt_df$AREA_LTE1_KM2[i] <- bt_raster |>
     cpa_from_raster(raster_units = "m", temperature_threshold = 1)
-  bt_df$AREA_LTE0_KM2[i] <- bt_raster |> 
+  bt_df$AREA_LTE0_KM2[i] <- bt_raster |>
     cpa_from_raster(raster_units = "m", temperature_threshold = 0)
-  bt_df$AREA_LTEMINUS1_KM2[i] <- bt_raster |> 
+  bt_df$AREA_LTEMINUS1_KM2[i] <- bt_raster |>
     cpa_from_raster(raster_units = "m", temperature_threshold = -1)
   bt_df$MEAN_GEAR_TEMPERATURE[i] <- mean(terra::values(bt_raster), na.rm = TRUE)
-  lt100_temp <- terra::mask(bt_raster, 
+  lt100_temp <- terra::mask(bt_raster,
                             lt100_strata,
                             touches = FALSE)
-  bt_df$MEAN_BT_LT100M[i] <- mean(terra::values(lt100_temp), na.rm = TRUE) 
-  
+  bt_df$MEAN_BT_LT100M[i] <- mean(terra::values(lt100_temp), na.rm = TRUE)
+
 }
 
 print("Writing nep_cpa.csv")
-write.csv(bt_df, "nep_cpa.csv", row.names=FALSE)
+write.csv(bt_df, paste( experiment_dir, "nep_cpa.csv", sep="/") , row.names=FALSE)
+
+# Interpolate gear temperature and write rasters for SEBS
+interpolation_wrapper(temp_data_path = ebs_nep_csv_path,
+                      proj_crs = proj_crs,
+                      cell_resolution = 5000, # 5x5 km grid resolution
+                      select_years = 1993:2019, # TODO: 2020 data is missing, leading to error.
+                      interp_variable = "gear_temperature",
+                      select_region = "sebs",
+                      methods = "Ste")
 
 # Calculate cold pool area
 print("Saving bottom temp files as var")
-bottom_temp_files <- list.files(here::here("output", "raster", "sebs", "gear_temperature"), 
+bottom_temp_files <- list.files(here::here("output", "raster", "sebs", "gear_temperature"),
                                 full.names = TRUE,
                                 pattern = "ste_")
 
@@ -84,8 +96,8 @@ ebs_layers <- akgfmaps::get_base_layers(select.region = "sebs", set.crs = proj_c
 
 print("lt100_strat")
 lt100_strata <- ebs_layers$survey.strata |>
-  dplyr::filter(Stratum %in% c(10, 20, 31, 32, 41, 42, 43)) |>
-  dplyr::group_by(SURVEY) |>
+  dplyr::filter(STRATUM %in% c(10, 20, 31, 32, 41, 42, 43)) |>
+  dplyr::group_by(SURVEY_DEFINITION_ID) |>
   dplyr::summarise()
 
 print("Listing bottom temp files")
@@ -93,21 +105,21 @@ for(i in 1:length(bottom_temp_files)) {
   print(i)
   bt_raster <- terra::rast(bottom_temp_files[i])
   bt_df$YEAR[i] <- as.numeric(gsub("[^0-9.-]", "", names(bt_raster))) # Extract year
-  bt_df$AREA_LTE2_KM2[i] <- bt_raster |> 
+  bt_df$AREA_LTE2_KM2[i] <- bt_raster |>
     cpa_from_raster(raster_units = "m", temperature_threshold = 2)
-  bt_df$AREA_LTE1_KM2[i] <- bt_raster |> 
+  bt_df$AREA_LTE1_KM2[i] <- bt_raster |>
     cpa_from_raster(raster_units = "m", temperature_threshold = 1)
-  bt_df$AREA_LTE0_KM2[i] <- bt_raster |> 
+  bt_df$AREA_LTE0_KM2[i] <- bt_raster |>
     cpa_from_raster(raster_units = "m", temperature_threshold = 0)
-  bt_df$AREA_LTEMINUS1_KM2[i] <- bt_raster |> 
+  bt_df$AREA_LTEMINUS1_KM2[i] <- bt_raster |>
     cpa_from_raster(raster_units = "m", temperature_threshold = -1)
   bt_df$MEAN_GEAR_TEMPERATURE[i] <- mean(terra::values(bt_raster), na.rm = TRUE)
-  lt100_temp <- terra::mask(bt_raster, 
+  lt100_temp <- terra::mask(bt_raster,
                             lt100_strata,
                             touches = FALSE)
-  bt_df$MEAN_BT_LT100M[i] <- mean(terra::values(lt100_temp), na.rm = TRUE) 
-  
+  bt_df$MEAN_BT_LT100M[i] <- mean(terra::values(lt100_temp), na.rm = TRUE)
+
 }
 
 print("Writing trawl csv")
-write.csv(bt_df, "trawl_cpa.csv", row.names=FALSE)
+write.csv(bt_df, paste(experiment_dir, "trawl_cpa.csv", sep = "/") , row.names=FALSE)
--- Figure_19_20/submit_R_script.sh
+++ Figure_19_20/submit_R_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --output=job_output_files/calculate_coldpool_areas_%j.job
-#SBATCH --job-name=Calculate_coldpool_areas
+#SBATCH --output=job_output_files/no_bias_correction_coldpool_%j.job
+#SBATCH --job-name=no_bias_correction_coldpool
 #SBATCH --time=300
 #SBATCH --partition=batch
 #SBATCH -n 1
@@ -8,5 +8,6 @@
 source $MODULESHOME/init/bash
 module load miniforge
 conda activate /work/role.medgrp/conda/r_env
+#conda activate /net3/e1n/miniconda3/envs/r_env # note that this env does not work as a batch job
 
 Rscript Calculate_Coldpool_Areas.r
